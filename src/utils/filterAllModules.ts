44d2ea03435605f870cbf8c847ce5fdecb30c264
francescoalterio <francescoalteriog@gmail.com>
2024-06-18 17:00:25 -0400

    refactor filterAllModules

--- src/utils/filterAllModules.ts
+++ src/utils/filterAllModules.ts
@@ -1,25 +1,21 @@
 import { opendir } from "node:fs/promises";
 import path from "node:path";
 
-type FilterAllModules = (currentPath: string, pathsToAnalyzed?: string[], pathsWithModules?: string[]) => Promise<string[]>
-
-export const filterAllModules: FilterAllModules = async function (
-  currentPath,
-  pathsToAnalyzed = [],
-  pathsWithModules = []
-) {
-
+export async function filterAllModules(
+  currentPath: string,
+  pathsToAnalyzed: string[] = [],
+  pathsWithModules: string[] = []
+): Promise<string[]> {
   try {
     const directoryContents = await opendir(currentPath);
     for await (const x of directoryContents) {
       if (x.isDirectory()) {
         const pathWithDir = path.join(currentPath, x.name);
-        if (x.name === "node_modules") pathsWithModules.push(pathWithDir)
+        if (x.name === "node_modules") pathsWithModules.push(pathWithDir);
         else pathsToAnalyzed.push(pathWithDir);
       }
     }
-  } catch (error) {
-  }
+  } catch (error) {}
 
   if (pathsToAnalyzed.length > 0) {
     return filterAllModules(
