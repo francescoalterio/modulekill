963f28a84c82237604b063a3c7cee49665f2b504
francescoalterio <francescoalteriog@gmail.com>
2024-06-19 09:39:46 -0400

    change title app

--- src/components/Title.ts
+++ src/components/Title.ts
@@ -2,7 +2,7 @@ import figlet from "figlet";
 import gradient from "gradient-string";
 
 export function Title() {
-  const title = figlet.textSync("Welcome to Modulekill", { font: "Big" });
+  const title = figlet.textSync("Modulekill", { font: "Big" });
   const colorizedTitle = gradient.summer.multiline(title);
 
   console.log(colorizedTitle);
